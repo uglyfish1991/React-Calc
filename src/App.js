c83fbae4d9e68864c549f2b5528d995d7cfe730b
Katy Long <katy.long@wearecodenation.com>
2022-08-09 17:18:56 +0100

    displays on screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+// const App = () => {
+//   const [persons, setPersons] = useState([
+//       { name: "Leon" },
+//       { name: "Jordan" },
+//   ]);
+//   const handleClick = (value) => {
+//       console.log(value);
+//   };
+//   return (
+//       <div>
+//           <Person name={persons[0].name} clickMe={handleClick} />
+//           <Person name={persons[1].name} clickMe={handleClick} />
+//       </div>
+//   );
+// };
+// const Person = (props) => {
+//   return <h1 onClick={() => props.clickMe({props.name})}>{props.name}</h1>;
+// }
+
+const App = () => {
+  const buttons =
+    [
+      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-'
+    ]
+
+  let [num, getNum] = useState([])
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>A Calculator</h1>
+      <div>
+        <Display num={num} />
+        <div className="buttonArea">
+          {buttons.map((calcButton, index) => {
+            return (
+              <button key={index} onClick={() => getNum(oldArray =>[...oldArray,calcButton])}>{calcButton}</button>
+            )
+          })}</div></div>
     </div>
-  );
+  )
 }
+
+const Display = (props) => {
+  return (
+    <div className="screen">
+      {props.num}
+    </div>
+  )
+}
+
+
 
 export default App;
